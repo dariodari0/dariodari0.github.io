a97dafde9adc238b31f928bbde1e7209044084a1
dariodari0 <dmarkowicz@outlook.com>
2017-09-13 23:19:32 +0200

    Add files via upload

---
+++ email.php
@@ -0,0 +1,11 @@
+<?php
+// Get Data	
+$name = strip_tags($_POST['name']);
+$email = strip_tags($_POST['email']);
+$message = strip_tags($_POST['message']);
+
+// Send Message
+mail( "dmarkowicz@outlook.com", "A mail from CV page!",
+"Name: $name\nEmail: $email\n\nMessage: $message\n",
+"From: dariuszmarkowicz.pl <website>" );
+?>
