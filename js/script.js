4cb359e44ff934386f8ce6f6261d22c8fba6895b
dariodari0 <dmarkowicz@outlook.com>
2017-09-27 22:48:51 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,24 @@
 /**
  * @author Dariusz
  */
-window.onload = function () {
-};
 /*
  * Obsługa panelu kontaktowego
  */
- $('ol > li:nth-child(1)').click(function() {
+ $("ol > li:nth-child(1)").click(function() {
  		$(this).fadeOut(300);
- 		$('body').css({"overflow":"hidden"});
- 		$('.contactForm, .contactMore, .overlay').fadeIn(300);
+ 		$("body").css({"overflow":"hidden"});
+ 		$(".contactForm, .contactMore, .overlay").fadeIn(300);
 	});
 
- $('.overlay').click(function() {
- 		$('.contactForm, .contactMore, .overlay').fadeOut(300);
- 		$('ol > li:nth-child(1)').fadeIn(300);
- 		$('body').css({"overflow":"auto"});
+ $(".overlay").click(function() {
+ 		$(".contactForm, .contactMore, .overlay").fadeOut(300);
+ 		$("ol > li:nth-child(1)").fadeIn(300);
+ 		$("body").css({"overflow":"auto"});
 	});
 /*
  * Obsługa menu
  */	
- $('ol > li:nth-child(2)').hover(function () {   
+ $("ol > li:nth-child(2)").hover(function () {   
  		$("ol > li:nth-child(2) > a").slideToggle(250);
         $("ul > li" ).slideToggle(250);
     });
@@ -38,7 +36,7 @@ $("a").click(function(){
     else if(theLastScroll === anchorClass) return;
     else {
     		var articleClass = $("article[class='"+ anchorClass +"']");
- 							$('html,body').animate({scrollTop: articleClass.offset().top - $(window).height()/12}, 950 );
+ 	$("html,body").animate({scrollTop: articleClass.offset().top - $(window).height()/12}, 950 );
     	}
     theLastScroll = anchorClass;
 });
