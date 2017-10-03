606556850cfc91b91e91cacadf5f088356604b5a
dariodari0 <dmarkowicz@outlook.com>
2017-10-03 15:51:39 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,44 +1,52 @@
 /**
  * @author Dariusz
  */
-/*
- * Obsługa panelu kontaktowego
- */
- $("ol > li:nth-child(1)").click(function() {
+$(document).ready(function()
+{
+	 $("ol > li:nth-child(1)").click(function() {
  		$(this).fadeOut(300);
+ 		$(".mainCointainer").addClass('blur');
  		$("body").css({"overflow":"hidden"});
  		$(".contactForm, .contactMore, .overlay").fadeIn(300);
 	});
 
  $(".overlay").click(function() {
  		$(".contactForm, .contactMore, .overlay").fadeOut(300);
+ 		$(".mainCointainer").removeClass('blur');
  		$("ol > li:nth-child(1)").fadeIn(300);
  		$("body").css({"overflow":"auto"});
 	});
 /*
  * Obsługa menu
  */	
- $("ol > li:nth-child(2)").hover(function () {   
+ $("ol > li:nth-child(2)").hover(function (e) {   
+ 		e.preventDefault;
  		$("ol > li:nth-child(2) > a").slideToggle(250);
         $("ul > li" ).slideToggle(250);
-    });
+    });	
+
+
 /*
  * Nawigowanie z poziomu menu
  */
-   
+
+
 var theLastScroll;
 
-$("a").click(function(){
-	var gotop = "goTop";
-	var anchorClass = $(this).attr("class");
-    
-    if (anchorClass === gotop) {$(window).scrollTop(0);}
-    else if(theLastScroll === anchorClass) return;
-    else {
-    		var articleClass = $("article[class='"+ anchorClass +"']");
- 	$("html,body").animate({scrollTop: articleClass.offset().top - $(window).height()/12}, 950 );
+   $("a").click(function(){
+   	  var gotop = "goTop";
+      var anchorClass = $(this).attr("class");
+      
+      if (anchorClass === gotop) {$(window).scrollTop(0);}
+      else if(!(theLastScroll === anchorClass))
+      {
+      		  var articleClass = $("article[class='"+ anchorClass +"']");
+              if (articleClass.length) {
+              var scrollToPosition = articleClass.offset().top - $(window).height()/12;
+              $("html,body").animate({scrollTop: scrollToPosition }, 950 );
+             }
     	}
     theLastScroll = anchorClass;
+    });
 });
-
  
