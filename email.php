794decd2f11a2a8af6a45e7b62c87c03fa13cea1
dariodari0 <dmarkowicz@outlook.com>
2017-10-12 15:58:12 +0200

    Update email.php

--- email.php
+++ email.php
@@ -9,6 +9,4 @@ mail( "dmarkowicz@outlook.com", "A mail from CV page!",
 "Name: $name\nEmail: $email\n\nMessage: $message\n",
 "From: dariodari0.github.io <website>" );
 
-// Back to previous page
-header("location:javascript://history.go(-1)");
 ?>
